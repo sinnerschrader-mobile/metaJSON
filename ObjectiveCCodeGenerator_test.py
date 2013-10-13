d08c5fa9bdd5d6a90336eb644be6d2d8e695bb73
François Benaiteau <francois.benaiteau@gmail.com>
2013-10-13 21:09:03 +0200

    ADD First test on getHeaderDescriptionString

---
+++ ObjectiveCCodeGenerator_test.py
@@ -0,0 +1,29 @@
+from ObjectiveCCodeGenerator import *
+
+
+import unittest
+
+class TestObjectiveCCodeGenerator(unittest.TestCase):
+	def setUp(self):
+		print('In setUp()')
+		self.gen = ObjectiveCCodeGenerator()
+
+	def tearDown(self):
+		print('In tearDown()')
+		del self.gen
+
+	def test_getHeaderDescriptionString(self):
+		result = self.gen.getHeaderDescriptionString("MyClass")
+		expected_result = """//
+//  MyClass.h
+//
+//  Created by MetaJSONParser.
+//  Copyright (c) 2013 SinnerSchrader Mobile. All rights reserved.
+
+"""
+		self.assertMultiLineEqual(result, expected_result)
+
+
+if __name__ == '__main__':
+		unittest.main()
+
