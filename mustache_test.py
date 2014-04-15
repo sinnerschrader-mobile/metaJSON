8195cb9b99b689d4b36bdb1e455d940bbc8a7656
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2014-04-15 18:47:00 +0200

    add sample test for mustache

---
+++ mustache_test.py
@@ -0,0 +1,34 @@
+import unittest
+
+import datetime
+import time
+import os
+import sys
+from pystache import Renderer
+
+# run nosetests -s mustache_test.py
+class TestMustache(unittest.TestCase):
+
+    def assert_content_file(self, filename, content):
+        with open(filename, 'r') as content_file:
+            expected_result = content_file.read()
+        self.assertMultiLineEqual(content, expected_result)
+
+    def test_mustache_machine_m(self):
+      templateFile = open(self.template_file_path("machine.m.mustache"), "r")
+      today = datetime.date.fromtimestamp(time.time())
+
+      hashParams = {"date": str(today.year), "machineClassName": "_MyObject", "projectPrefix": "S2M", "humanClassName": "MyObject", "variableName": ""}
+
+      result = Renderer().render(templateFile.read(), hashParams)
+      print result
+      # self.assert_content_file(self.test_file_path + "/_S2MSenderJSONObject.h", result)
+
+    def template_file_path(self, filename) :
+        templatePath = os.path.realpath( __file__ )
+        templatePath = templatePath.replace(os.path.basename( __file__ ), 'templates')
+        return os.path.join(templatePath, filename)
+
+
+if __name__ == '__main__':
+    unittest.main()
