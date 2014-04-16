5922e3e193890c9bdd00b4a81263826026b6d452
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2014-04-16 18:28:29 +0200

    add new extension to human file

--- test_data/test_class/S2MSenderJSONObject.m
+++ test_data/test_class/S2MSenderJSONObject.m
@@ -5,6 +5,11 @@
 //  Copyright (c) 2014 SinnerSchrader Mobile. All rights reserved.
 
 #import "S2MSenderJSONObject.h"
+
+@interface S2MSenderJSONObject ()
+
+@end
+
 @implementation S2MSenderJSONObject
 
 @end
