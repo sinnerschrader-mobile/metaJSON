9634deecf587c8e07f2f2dd5d8abc37d18eebad9
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2014-04-23 18:17:59 +0200

    Testing ProductDetailsJSONObject.h file

--- readJSON_test.py
+++ readJSON_test.py
@@ -13,4 +13,6 @@ class TestReadJSON(unittest.TestCase):
         os.system("python readJSON.py -i " + json_file)
         # ignoring template files
         os.system("rm -rf src/Utilities")
-        return os.system("diff -r src/ " + output_dir)
+        # return os.system("diff -r src/ " + output_dir)
+        common_file = "/AbstractInterfaceFiles/_ProductDetailJSONObject.h"
+        return os.system("diff -r -B src" + common_file + " " + output_dir + common_file)
