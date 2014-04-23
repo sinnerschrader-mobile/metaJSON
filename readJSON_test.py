80595e2053f7f534d386af4115916f304eb55f79
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2014-04-23 09:51:57 +0200

    Fixes readJSON_test

--- readJSON_test.py
+++ readJSON_test.py
@@ -13,4 +13,4 @@ class TestReadJSON(unittest.TestCase):
         os.system("python readJSON.py -i " + json_file)
         # ignoring template files
         os.system("rm -rf src/Utilities")
-        return os.system("diff src/ " + test_data/product)
+        return os.system("diff src/ " + output_dir)
