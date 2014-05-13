3aa870c701092bf9727ce49c0911591f5378d31e
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2014-05-13 16:11:06 +0200

    reorganise files into package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='metaJSON',
+      version='1.0.0',
+      description='metaJSON provides a meta language to generate object models for several languages. The generated classes can easily be used for the client-server communication.',
+      url='https://github.com/sinnerschrader-mobile/metaJSON.git',
+      license='MIT',
+      scripts=['readJSON.py']
+      )
