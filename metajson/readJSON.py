1569eba6a1dac645fe3fed69760aadcbb7963d36
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2014-05-13 16:49:31 +0200

    update readJSON with main method

--- metajson/readJSON.py
+++ metajson/readJSON.py
@@ -29,28 +29,29 @@ import os
 import glob
 import shutil
 import sys, getopt
+
 from JSONScheme import *
 from ObjectiveCCodeGenerator import *
 from JavaCodeGenerator import *
 
+def main(argv=sys.argv):
+    jsonfiles = []
+    inputfile = 'do you have files?'
+    projectPrefix = ''
+    target = 'iOS'
+    dirPathToSaveCodes = './src'
+    objectSuffix = "JSONObject"
 
-jsonfiles = []
-inputfile = 'do you have files?'
-projectPrefix = ''
-target = 'iOS'
-dirPathToSaveCodes = './src'
-objectSuffix = "JSONObject"
+    usageString = '\nreadJSON.py [ -p | -t | -o | -s ] [-i]\n'
+    usageString += 'Options:\n'
+    usageString += '  -h, --help            shows help\n'
+    usageString += '  -p, --prefix=         project prefix (default: S2M)\n'
+    usageString += '  -s, --suffix=         classname suffix (default: JSONObject). Use "-s false" for no suffix\n'
+    usageString += '  -t, --target=         target platform iOS or Android (default: iOS)\n'
+    usageString += '  -i, --input=          meta-JSON file to read\n'
+    usageString += '  -o, --output=         ouput path of generated source codes\n'
 
-usageString = '\nreadJSON.py [ -p | -t | -o | -s ] [-i]\n'
-usageString += 'Options:\n'
-usageString += '  -h, --help            shows help\n'
-usageString += '  -p, --prefix=         project prefix (default: S2M)\n'
-usageString += '  -s, --suffix=         classname suffix (default: JSONObject). Use "-s false" for no suffix\n'
-usageString += '  -t, --target=         target platform iOS or Android (default: iOS)\n'
-usageString += '  -i, --input=          meta-JSON file to read\n'
-usageString += '  -o, --output=         ouput path of generated source codes\n'
-
-if __name__ == "__main__":
+    print("Executing metajson version %s." % __version__)
     argv = sys.argv[1:]
     try:
         opts, args = getopt.getopt(argv,"ho:p:s:t:i:",["prefix=","suffix=","target=","input=","output="])
@@ -89,115 +90,117 @@ if __name__ == "__main__":
         if addFiles :
             jsonfiles.append(arg)
 
-if target == 'none' :
-    print 'error - target platform is not defined'
-    print(usageString)
-    sys.exit()
+    if target == 'none' :
+        print 'error - target platform is not defined'
+        print(usageString)
+        sys.exit()
 
-if len(jsonfiles) == 0 :
-    print 'error - meta JSON file is not defined'
-    print(usageString)
-    sys.exit()
+    if len(jsonfiles) == 0 :
+        print 'error - meta JSON file is not defined'
+        print(usageString)
+        sys.exit()
 
 
 
-templatePath = os.path.abspath( __file__ )
-templatePath = templatePath.replace('/readJSON.py', '/templates')
+    templatePath = os.path.abspath( __file__ )
+    templatePath = templatePath.replace('/readJSON.py', '/templates')
 
-if not os.path.exists(dirPathToSaveCodes):
-    os.makedirs(dirPathToSaveCodes)
+    if not os.path.exists(dirPathToSaveCodes):
+        os.makedirs(dirPathToSaveCodes)
+    iOS = False
+    Android = False
+    if target == 'iOS' :
+        iOS = True
+    elif target == 'Android' :
+        Android = True
+    else :
+        print 'error - unknown target platform : ' + target
+        print(usageString)
+        sys.exit()
+
+
+    hasError = False
+
+    if iOS :
+        if dirPathToSaveCodes.endswith("/") :
+                dirPathToSaveCodes = dirPathToSaveCodes[:-1]
+
+        if os.path.exists(dirPathToSaveCodes+ "/abstractInterfaceFiles"):
+            shutil.rmtree(dirPathToSaveCodes + "/abstractInterfaceFiles")
+
+        templateCodeGen = TemplateCodeGenerator()
+        templateCodeGen.templatePath = templatePath
+        templateCodeGen.projectPrefix = projectPrefix
+        templateCodeGen.dirPath = dirPathToSaveCodes
+        templateCodeGen.writeTemplates()
+
+    JSONScheme.projectPrefix = projectPrefix
+    JSONScheme.objectSuffix = objectSuffix
+
+    print "\nGenerate source codes for " + target + ", with Project Prefix \'" + projectPrefix + "\' and suffix \'" + objectSuffix + "\'"
+    print "Output path : \'" + dirPathToSaveCodes + "\'\n"
+
+    for filePath in jsonfiles :
+        print "read " + filePath + " to parse ...."
+
+        jsonObj = openFileAndParseJSON(filePath)
+
+        if type(jsonObj) == list :
+            for dic in jsonObj :
+                schemeObj = JSONScheme()
+                schemeObj.projectPrefix = projectPrefix
+                schemeObj.objectSuffix = objectSuffix
+                if schemeObj.parseDictionary(dic) == False:
+                    hasError = True
+                    break
+
+        elif type(jsonObj) == dict :
+            schemeObj = JSONScheme()
+            if schemeObj.parseDictionary(jsonObj) == False :
+                hasError = True
+
+        else :
+            hasError = True
+            print "error : no JSON Scheme"
+            break;
+
+        if hasError :
+            print "error - Fail to make scheme object."
+            break;
+
+        if hasError :
+            break
+
+        if not hasError:
+            codeGen = 0
+
+            if Android :
+                codeGen = JavaCodeGenerator()
+            else :
+                codeGen = ObjectiveCCodeGenerator()
+
+            codeGen.projectPrefix = projectPrefix
+            codeGen.dirPath = dirPathToSaveCodes
+            codeGen.objectSuffix = objectSuffix
+            allSchemes = JSONScheme.JSONSchemeDic
+            codeGen.JSONSchemeDic = allSchemes
+
+            rootDic = allSchemes["ROOT"]
+            allRootKeys = rootDic.keys()
+            for typeName in rootDic :
+                obj = rootDic[typeName];
+                if obj.isNaturalType() == False :
+                    codeGen.make(obj)
 
 def openFileAndParseJSON(filePath):
-	f = open(filePath)
+    f = open(filePath)
 
-	try:
-	    obj = json.load(f)
-	finally:
-	    f.close()
+    try:
+        obj = json.load(f)
+    finally:
+        f.close()
 
-	return obj
+    return obj
 
-iOS = False
-Android = False
-if target == 'iOS' :
-    iOS = True
-elif target == 'Android' :
-    Android = True
-else :
-    print 'error - unknown target platform : ' + target
-    print(usageString)
-    sys.exit()
-
-
-hasError = False
-
-if iOS :
-    if dirPathToSaveCodes.endswith("/") :
-            dirPathToSaveCodes = dirPathToSaveCodes[:-1]
-
-    if os.path.exists(dirPathToSaveCodes+ "/abstractInterfaceFiles"):
-        shutil.rmtree(dirPathToSaveCodes + "/abstractInterfaceFiles")
-
-    templateCodeGen = TemplateCodeGenerator()
-    templateCodeGen.templatePath = templatePath
-    templateCodeGen.projectPrefix = projectPrefix
-    templateCodeGen.dirPath = dirPathToSaveCodes
-    templateCodeGen.writeTemplates()
-
-JSONScheme.projectPrefix = projectPrefix
-JSONScheme.objectSuffix = objectSuffix
-
-print "\nGenerate source codes for " + target + ", with Project Prefix \'" + projectPrefix + "\' and suffix \'" + objectSuffix + "\'"
-print "Output path : \'" + dirPathToSaveCodes + "\'\n"
-
-for filePath in jsonfiles :
-    print "read " + filePath + " to parse ...."
-
-    jsonObj = openFileAndParseJSON(filePath)
-
-    if type(jsonObj) == list :
-        for dic in jsonObj :
-            schemeObj = JSONScheme()
-            schemeObj.projectPrefix = projectPrefix
-            schemeObj.objectSuffix = objectSuffix
-            if schemeObj.parseDictionary(dic) == False:
-                hasError = True
-                break
-
-    elif type(jsonObj) == dict :
-        schemeObj = JSONScheme()
-        if schemeObj.parseDictionary(jsonObj) == False :
-            hasError = True
-
-    else :
-        hasError = True
-        print "error : no JSON Scheme"
-        break;
-
-    if hasError :
-        print "error - Fail to make scheme object."
-        break;
-
-    if hasError :
-        break
-
-    if not hasError:
-        codeGen = 0
-
-        if Android :
-            codeGen = JavaCodeGenerator()
-        else :
-            codeGen = ObjectiveCCodeGenerator()
-
-        codeGen.projectPrefix = projectPrefix
-        codeGen.dirPath = dirPathToSaveCodes
-        codeGen.objectSuffix = objectSuffix
-        allSchemes = JSONScheme.JSONSchemeDic
-        codeGen.JSONSchemeDic = allSchemes
-
-        rootDic = allSchemes["ROOT"]
-        allRootKeys = rootDic.keys()
-        for typeName in rootDic :
-            obj = rootDic[typeName];
-            if obj.isNaturalType() == False :
-                codeGen.make(obj)
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
