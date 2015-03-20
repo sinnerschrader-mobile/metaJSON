836833c37ef987ddda8b5db393d3fa720b3cd026
Martin Bober <mbo@sinnerschrader-mobile.com>
2015-03-20 16:22:55 +0100

    Improved test portability to fix sinnerschrader-mobile/metaJSON#28

--- test/readJSON_test.py
+++ test/readJSON_test.py
@@ -1,6 +1,10 @@
-import unittest
-import os
 import datetime
+import difflib
+import os
+import re
+import shutil
+import sys
+import unittest
 
 class TestReadJSON(unittest.TestCase):
     def setUp(self):
@@ -8,32 +12,59 @@ class TestReadJSON(unittest.TestCase):
 
     def test_command(self):
         exit_status = self.execute_script("samples/product.json", "test/data/product")
-        self.assertEqual(exit_status, 0) # no difference
+        self.assertTrue(exit_status)
 
     def execute_script(self, json_file, output_dir):
         os.system("python -m metajson -p '' -i " + json_file)
         # ignoring template files
-        os.system("rm -rf ./src/APIParser")
-        os.system("rm -rf ./src/Utilities")
-        # return os.system("diff -r src/ " + output_dir)
-        # common_file = "/AbstractInterfaceFiles/_ProductDetailJSONObject.h"
-        # common_file = "/AbstractInterfaceFiles/_ProductDetailJSONObject.h"
-        # common_file = "/AbstractInterfaceFiles/_SenderGroupJSONObject.h"
-        # common_file = "/AbstractInterfaceFiles/_SenderList2JSONObject.h"
-        # common_file = "/AbstractInterfaceFiles/_SenderListJSONObject.m"
-        # common_file = "/AbstractInterfaceFiles/_SenderList2JSONObject.m"
-        # common_file = "/AbstractInterfaceFiles/_SenderJSONObject.m"
-        # common_file = "/AbstractInterfaceFiles/_SenderGroupJSONObject.m"
-        
-        # All timestamps in reference files should be 2014, so we need to tweak our generated files to match them.
-        os.system("find src -type f -exec sed -r --in-place \"s/%d/2014/g\" {} +" % (datetime.datetime.now().year, ) )
-        return os.system("diff -r -w -b -B src " + output_dir)
-        # return os.system("diff -r -w -b -B src" + common_file + " " + output_dir + common_file)
+        shutil.rmtree(os.path.join("src", "APIParser"))
+        shutil.rmtree(os.path.join("src", "Utilities"))
+        return self.compare_dirs("src", output_dir, fix_date = True)
 
     def test_yaml_input(self):
-        json_file = "samples/product.json"
-        yaml_file = "samples/product.yaml"
-        os.system("python -m metajson -i " + json_file + " -o src/json")
-        os.system("python -m metajson -i " + yaml_file + " -o src/yaml")
-        exit_status = os.system("diff -r -w -b -B src/json src/yaml")
-        self.assertEqual(exit_status, 0) # no difference
+        json_file = os.path.join("samples", "product.json")
+        yaml_file = os.path.join("samples", "product.yaml")
+        json_out_dir = os.path.join("src", "json")
+        yaml_out_dir = os.path.join("src", "yaml")
+        os.system("python -m metajson -i " + json_file + " -o " + json_out_dir)
+        os.system("python -m metajson -i " + yaml_file + " -o " + yaml_out_dir)
+        exit_status = self.compare_dirs(json_out_dir, yaml_out_dir)
+        self.assertTrue(exit_status)
+
+    def compare_dirs(self, dir_a, dir_b, fix_date = False):
+        files_in_a = []
+        for (dirpath, subdirs, filenames) in os.walk(dir_a):
+            current_dir = dirpath
+            for filename in filenames:
+                cur_file_path_a = os.path.join(dirpath,filename)
+                cur_file_path_b = cur_file_path_a.replace(dir_a, dir_b, 1)
+                if not os.path.isfile(cur_file_path_b):
+                    print "%s not found in %s." % (current_file_path_a, dir_b)
+                    return False
+                files_in_a += [cur_file_path_a]
+                content_a = "foo"
+                content_b = "bar"
+                cur_file_a = open(cur_file_path_a, 'r')
+                cur_file_b = open(cur_file_path_b, 'r')
+                try: 
+                    content_a = cur_file_a.read()
+                    if fix_date:
+                        # All timestamps in reference files should be 2014, so we need to tweak our generated files to match them.
+                        content_a = content_a.replace(str(datetime.datetime.now().year), "2014")
+                    content_a = re.sub("\s", "", content_a)
+                    content_b = re.sub("\s", "", cur_file_b.read())
+                finally:
+                    cur_file_a.close()
+                    cur_file_b.close()
+                if content_a != content_b:
+                    for line in difflib.unified_diff(content_a, content_b, fromfile=cur_file_path_a, tofile=cur_file_path_b):
+                        sys.stdout.write(line)
+                    return False
+        for (dirpath, subdirs, filenames) in os.walk(dir_b):
+            for filename in filenames:
+                current_file_name = os.path.join(dirpath, filename)
+                if not (current_file_name.replace(dir_b, dir_a, 1) in files_in_a):
+                  print "%s not found in %s" % (current_file_name, dir_a)
+                  return False
+        return True
+
